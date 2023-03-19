6eefc2c57d62f6fa966186e73f64cd6e906bf782
Taegyeong Lee <hanarotg@naver.com>
2023-03-19 17:40:37 +0900

    feat: 랜딩 페이지 1차 업데이트

--- App.tsx
+++ App.tsx
@@ -5,16 +5,19 @@
  * @format
  */
 
-import React from 'react';
+import React, {cloneElement, useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  ImageBackground,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
 
 import {
@@ -25,6 +28,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import QuizStartSection from './Components/QuizStartSection';
+import DeviceInfo from 'react-native-device-info';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -33,23 +37,76 @@ type SectionProps = PropsWithChildren<{
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [deviceUniqueID, setdeviceUniqueID] = useState("")
+  const [listOfSubject, setListOfSubject] = useState([])
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    
+    // get subject list of subject
+    fetch('https://www.iterview.site/graphql', 
+      { method : 'POST',
+        headers: {"Content-Type": "application/json"},
+        body : JSON.stringify({query: 
+          `query ExampleQuery {
+          getListOfSubject {
+            name, quizId
+          }
+        }`
+      })})
+        .then(async (response) => {
+          const data = await response.json()
+          setListOfSubject(data.data.getListOfSubject)
+        }
+    )
+    
+    // get unique device id
+    DeviceInfo.getUniqueId().then((getUniqueId) => {
+      setdeviceUniqueID(getUniqueId)
+    })
+
+  }, [])
+
   return (
     <SafeAreaView style={{flex: 1}}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={{flex: 1}}>
-        <View style={{padding: 10}}>
-        <QuizStartSection />
-
+      <View className='px-2 flex-row justify-between' style={{ borderBottomWidth: 1, borderColor: '#e0e0e0'}}>
+        <View className='flex-row items-center'>
+          <Image source={require('./Components/Images/Logo128.png')} style={{width: 48, height: 48}}/>
         </View>
+        <View className='items-center justify-center'>
+          <TouchableOpacity className='w-8 h-8 bg-slate-300 rounded-full'></TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView className="p-4">
+        <View className="mt-2">
+          <Text className="text-xl text-gray-800 font-bold">오늘도 상큼한{'\n'}IT터뷰 준비하세요.</Text>
+      </View>
+
+       {listOfSubject && listOfSubject.map((element, index) => {
+        return (
+          <View key={index} className="my-4 bg-blue-100 rounded-lg">
+          <ImageBackground className="h-24" imageStyle={{borderTopLeftRadius: 10, borderTopRightRadius: 10}} source={{uri : element.Image ?? "https://wallpapers.microsoft.design/images/grid-29-@1x.jpg"}} resizeMode='cover'>
+          </ImageBackground>
+          <View className='p-4'>
+            <Text className='text-gray-900'>{element.description ?? "여기는 부제목, 변수명은 description입니다."}</Text>
+            <Text className='text-2xl font-bold'>{element.name ?? "여기는 제목, 변수명은 Name 입니다."}</Text>
+            <TouchableOpacity className='mt-6 p-2 bg-blue-500 rounded-lg'>
+              <Text className='text-lg text-blue-100 text-center'>퀴즈 풀기</Text>
+            </TouchableOpacity>
+            </View>
+          </View>
+        )
+       })}
+
+       <View className='p-4 bg-blue-100 rounded-lg '>
+          <Text className='font-bold text-gray-900'>남선생께 드리는 편지</Text>
+          <Text className="mt-4 text-gray-900">기기 고유 ID : {deviceUniqueID && deviceUniqueID}</Text>
+          <Text className="mt-4 text-gray-900">주제별 배경화면 변수 명은 Image 입니다. 이미지 주소를 String 타입으로 받습니다. 예: https://img-url.abc.de/fg 반환하는 이미지 주소가 없을 시 기본 배경화면으로 설정됩니다. </Text> 
+        </View>
+
       </ScrollView>
     </SafeAreaView>
   );
--- Components/QuizStartSection.tsx
+++ Components/QuizStartSection.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Text, TouchableOpacity, View } from 'react-native'
 
-function QuizStartSection(): JSX.Element {
+function QuizStartSection({ title, subtitle}): JSX.Element {
+
     return(
         <TouchableOpacity style={{ backgroundColor:"#c8c8c8", borderRadius: 15}}>
             <View>
 
             </View>
             <View style={{padding: 10}}>
-            <Text>PM와 디자이너를 위한</Text>
-            <Text style={{ fontSize: 24}}>IT 상식 퀴즈</Text>
+            <Text>{subtitle && subtitle}</Text>
+            <Text style={{ fontSize: 24}}>{title && title}</Text>
             </View>
             
         </TouchableOpacity>
