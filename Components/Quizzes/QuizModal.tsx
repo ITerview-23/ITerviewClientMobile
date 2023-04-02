9e865abf481c9ac25de4bb7898d068d6fefa83e9
Taegyeong Lee <hanarotg@naver.com>
2023-04-02 10:13:46 +0900

    [FEAT] 정답 보기 기능 구현

--- Components/Quizzes/QuizModal.tsx
+++ Components/Quizzes/QuizModal.tsx
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   SafeAreaView,
   KeyboardAvoidingView,
+  ScrollView,
 } from 'react-native';
 
 interface Props {
@@ -70,6 +71,32 @@ function QuizModal({visible, onClose}): JSX.Element {
       });
   };
 
+  // 모범 답안을 표기하는 함수
+  const showAnswer = () => {
+    fetch('https://www.iterview.site/graphql', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        query: `
+          query ExampleQuery($quizId: Int!) {
+            getAnswer(quizId: $quizId)
+          }
+        `,
+        variables: {quizId: 1},
+      }),
+    })
+      .then(async response => {
+        const data = await response.json();
+        return data.data.getAnswer.toString();
+      })
+      .then(answer => {
+        setQuizMessage(answer);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     // get quiz
     fetch('https://www.iterview.site/graphql', {
@@ -121,77 +148,83 @@ function QuizModal({visible, onClose}): JSX.Element {
           </View>
         </View>
 
-        <KeyboardAvoidingView>
-          <View className="px-4 pt-4 flex-row justify-betweens">
-            <View className="justify-center">
-              <Text className="text-2xl text-gray-900 font-bold">
-                데일리 퀴즈
-              </Text>
-              <Text className="text-gray-800">
-                {new Date().toLocaleString('ko-KR', {
-                  month: 'short',
-                  day: 'numeric',
-                })}
-              </Text>
-            </View>
-          </View>
-
-          <View className="p-4">
-            <View className="p-4 py-10 bg-blue-200 rounded-lg">
-              <View className="flex-row flex-wrap items-center justify-center">
-                {quiz &&
-                  quiz.quizInfo.map((value, index) => {
-                    if (value.length == 0) {
-                      return (
-                        <TextInput
-                          key={index}
-                          onChangeText={text => {
-                            changeQuizInputByIndex(index, text);
-                          }}
-                          editable={true}
-                          numberOfLines={1}
-                          className="pb-2 px-2 m-2 rounded-lg border-b-2 border-blue-500 bg-gray-100 text-lg"
-                        />
-                      );
-                    } else {
-                      return (
-                        <Text key={index} className="text-lg text-gray-900">
-                          {value}
-                        </Text>
-                      );
-                    }
+        <ScrollView>
+          <KeyboardAvoidingView>
+            <View className="px-4 pt-4 flex-row justify-betweens">
+              <View className="justify-center">
+                <Text className="text-2xl text-gray-900 font-bold">
+                  데일리 퀴즈
+                </Text>
+                <Text className="text-gray-800">
+                  {new Date().toLocaleString('ko-KR', {
+                    month: 'short',
+                    day: 'numeric',
                   })}
+                </Text>
               </View>
             </View>
-          </View>
 
-          <View className="p-4">
-            <TouchableOpacity
-              onPress={() => {
-                checkAnswer();
-              }}
-              className="p-2 bg-blue-500 rounded-lg items-center">
-              <Text className="text-lg text-gray-100">제출하기</Text>
-            </TouchableOpacity>
-          </View>
+            <View className="p-4">
+              <View className="p-4 py-10 bg-blue-200 rounded-lg">
+                <View className="flex-row flex-wrap items-center justify-center">
+                  {quiz &&
+                    quiz.quizInfo.map((value, index) => {
+                      if (value.length == 0) {
+                        return (
+                          <TextInput
+                            key={index}
+                            onChangeText={text => {
+                              changeQuizInputByIndex(index, text);
+                            }}
+                            editable={true}
+                            numberOfLines={1}
+                            className="pb-2 px-2 m-2 rounded-lg border-b-2 border-blue-500 bg-gray-100 text-lg"
+                          />
+                        );
+                      } else {
+                        return (
+                          <Text key={index} className="text-lg text-gray-900">
+                            {value}
+                          </Text>
+                        );
+                      }
+                    })}
+                </View>
+              </View>
+            </View>
 
-          <View className="p-4">
-            <TouchableOpacity
-              onPress={() => {
-                checkAnswer();
-              }}
-              className="p-2 bg-blue-200 rounded-lg items-center">
-              <Text className="text-lg text-blue-600">모범 답안</Text>
-            </TouchableOpacity>
-          </View>
+            <View className="px-4">
+              <TouchableOpacity
+                onPress={() => {
+                  checkAnswer();
+                }}
+                className="p-4 bg-blue-500 rounded-lg items-center">
+                <Text className="text-lg text-gray-100">제출하기</Text>
+              </TouchableOpacity>
+            </View>
 
-          <View className="p-4 items-center">
-            <Text className="text-blue-900">
-              {isQuizAnswer &&
-                (isQuizAnswer == true ? '정답입니다!' : '오답입니다!')}
-            </Text>
-          </View>
-        </KeyboardAvoidingView>
+            <View className="px-4 py-2">
+              <TouchableOpacity
+                onPress={() => {
+                  showAnswer();
+                }}
+                className="p-2 bg-gray-100 rounded-lg items-center">
+                <Text className="text-lg text-blue-500">정답 보기</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View className="p-4 items-center">
+              {isQuizAnswer && isQuizAnswer == true ? (
+                <Text className="text-blue-900">{'정답입니다!'}</Text>
+              ) : (
+                <Text className="text-blue-900">{'땡!'}</Text>
+              )}
+              {quizMessage && (
+                <Text className="text-blue-900">정답 : {quizMessage}</Text>
+              )}
+            </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
       </SafeAreaView>
     </Modal>
   );
