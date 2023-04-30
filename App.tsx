7f06e2ef7109be12cc1e208f04d874a60ac30f51
Taegyeong Lee <hanarotg@naver.com>
2023-04-30 18:12:56 +0900

    [FEAT] 네이비게이션 파일 스타일링 목록에 추가

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// Stack Screens
+import MainScreen from './MainScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <SafeAreaView className="bg-white" style={{flex: 1}}>
       {/* 안드로이드 상태바 디자인 통일 */}
       <StatusBar backgroundColor={'#FFFFFF'} barStyle={'dark-content'} />
-      <NavigationContainer></NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Main" component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
